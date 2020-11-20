9d6ea4eaf2d570134ebdf40b474513269084094c
albertricart <albertricartt@gmail.com>
2020-11-20 12:20:14 +0100

    base de datos actualizada

--- BD/recomercem.sql
+++ BD/recomercem.sql
@@ -2,8 +2,8 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:pma__favorite
--- Generation Time: Nov 13, 2020 at 02:01 PM
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2020 at 12:16 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -20,18 +20,13 @@ SET time_zone = "+00:00";
 --
 -- Database: `recomercem`
 --
-CREATE DATABASE IF NOT EXISTS `recomercem` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `recomercem`;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `comerc`
 --
--- Creation: Nov 13, 2020 at 12:58 PM
---
 
-DROP TABLE IF EXISTS `comerc`;
 CREATE TABLE `comerc` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
@@ -45,113 +40,72 @@ CREATE TABLE `comerc` (
   `tickets_reservado` smallint(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELATIONSHIPS FOR TABLE `comerc`:
---
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `juego`
 --
--- Creation: Nov 13, 2020 at 11:35 AM
---
 
-DROP TABLE IF EXISTS `juego`;
 CREATE TABLE `juego` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL
+  `nombre` varchar(50) NOT NULL,
+  `puntuacion_max` int(11) NOT NULL,
+  `orden` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `juego`:
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `oferta`
 --
--- Creation: Nov 13, 2020 at 12:17 PM
---
 
-DROP TABLE IF EXISTS `oferta`;
 CREATE TABLE `oferta` (
   `id` int(11) NOT NULL,
-  `id_comerc` int(11) NOT NULL
+  `id_comerc` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `valor` varchar(50) NOT NULL,
+  `puntos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `oferta`:
---   `id_comerc`
---       `comerc` -> `id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `partida`
 --
--- Creation: Nov 13, 2020 at 12:12 PM
---
 
-DROP TABLE IF EXISTS `partida`;
 CREATE TABLE `partida` (
   `id_juego` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `puntuacion_tot` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELATIONSHIPS FOR TABLE `partida`:
---   `id_juego`
---       `juego` -> `id`
---   `id_usuario`
---       `usuario` -> `id`
---
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `ticket`
 --
--- Creation: Nov 13, 2020 at 12:47 PM
---
 
-DROP TABLE IF EXISTS `ticket`;
 CREATE TABLE `ticket` (
   `id` char(40) NOT NULL,
   `id_comerc` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `fecha_emision` int(10) UNSIGNED NOT NULL,
   `fecha_canje` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `ticket`:
---   `id_comerc`
---       `comerc` -> `id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `usuario`
 --
--- Creation: Nov 13, 2020 at 11:34 AM
--- Last update: Nov 13, 2020 at 11:30 AM
---
 
-DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT 0
+  `password` varchar(128) NOT NULL,
+  `puntuacion` smallint(5) UNSIGNED NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `usuario`:
---
 
 --
 -- Indexes for dumped tables
@@ -188,7 +142,8 @@ ALTER TABLE `partida`
 --
 ALTER TABLE `ticket`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_comerc` (`id_comerc`);
+  ADD KEY `id_comerc` (`id_comerc`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indexes for table `usuario`
@@ -227,7 +182,8 @@ ALTER TABLE `partida`
 -- Constraints for table `ticket`
 --
 ALTER TABLE `ticket`
-  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_comerc`) REFERENCES `comerc` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_comerc`) REFERENCES `comerc` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
