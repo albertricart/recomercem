54691fa2c23615b1872391b13ac11fa40ca420b7
magomo <mgoncevatt.cep@gmail.com>
2021-01-21 13:32:07 +0100

    20210121.1332

--- DEV/web/languages/summary_cat.php
+++ DEV/web/languages/summary_cat.php
@@ -17,7 +17,7 @@ $jsonTraslate = '{
     "obtenido":"@@expresion !!! ... en el joc \"@@juego\" no has obtingut punts",
     "almacenado":"El total acumulat és de @@total punts.",
     "jugados":"Encara no has jugat a tots els jocs disponibles (@@jugados).",
-    "suficiente":"La quantitat de punts obtinguts encara no és suficient per obtenir el Tiquet de descompte. Intenta noves partides per superar els @@points2ticket punts.",
+    "suficiente":"No has jugat tots els jocs o la quantitat de punts obtinguts encara no és suficient per obtenir el Tiquet de descompte. Intenta noves partides per superar els @@points2ticket punts.",
     "getticket":"Demana el Tiquet de Descompte",
     "resumen":"El resum actual de les teves partides és:"
 }';
@@ -25,7 +25,7 @@ $jsonTraslate = '{
 $jsonChange = '{"expresion":["Ups", "Bé", "Felicitats"],
     "obtenido":"@@expresion !!! ... en el joc \"@@juego\" has obtingut @@puntos punts",
     "jugados":"Ja has jugat a tots els jocs disponibles (@@jugados).",
-    "suficiente":"No has jugat tots els jocs o la quantitat de punts obtinguts encara no és suficient per obtenir el Tiquet de descompte. Pots obtenir-lo en el teu email pressionant el següent botó.",
+    "suficiente":"La quantitat de punts obtinguts és suficient per obtenir el Tiquet de descompte. Pots obtenir-lo en el teu email pressionant el següent botó.",
     "enviado":"La quantitat de punts obtinguts és suficient per obtenir el Tiquet de descompte però el mateix ja ha estat enviat per email. Si no us ha arribat o tens algun problema, si us plau envia\'ns un email a incidencias@recomercem.es."
 }';
 
